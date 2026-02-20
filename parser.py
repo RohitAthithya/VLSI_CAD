70109e62de736ee9229a835ea7f83e9b46438378
Rohit Athithya <rohitathithya@gmail.com>
2026-02-20 11:14:58 -0700

    parser.py >> stub for actual class to be implemented

--- parser.py
+++ parser.py
@@ -7,4 +7,16 @@ def print_output():
     print("hello")
 
 
+# phase 1: print the fan in and fan out info
+class node:
+    # each gate is a node
+
+    # input list - multiple fan ins
+    def __init__(self):
+        self.inputs = list()
+        self.outputs = list()
+
+    # output - multiple fan outs
+
+
 print_output()
